7322d383fee79bfbdd5d00d43150236e31c7e8ef
khanifzyen <khanif.zyen@gmail.com>
2025-12-22 14:28:03 +0700

    update

--- presensimengajar_app/test-require.js
+++
@@ -1,6 +0,0 @@
-try {
-    require('react-native-worklets/plugin');
-    console.log('Successfully required react-native-worklets/plugin');
-} catch (error) {
-    console.error('Failed to require react-native-worklets/plugin:', error);
-}
